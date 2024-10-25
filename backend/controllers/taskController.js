353c6f3bfcec091aeae1acde18c0708e22003a19
Vishnu Kumar <vermaji1891@gmail.com>
2024-10-25 23:34:25 +0530

    added deleted controller

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -47,6 +47,23 @@ exports.getTaskById = async (req, res) => {
 }
 
 
+exports.deleteTaskById = async (req, res) => {
+    const {id} = req.params;
+    try{
+        const task = await Task.findByIdAndDelete(id);
+        if(!task) {
+            return res.status(404).json({message: "Task Not Found!"})
+        }
+        res.status(200).json({message: "Task Deleted Successfully!", task: task})
+    }
+    catch(error) {
+        console.log("error caught by geting deleted single task", error)
+        res.status(500).json({message: "Internal Server Error!"})
+    }
+}
+
+
+
 
 
 
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createTask, getAllTasks, getTaskById } = require("../controllers/taskController");
+const { createTask, getAllTasks, getTaskById, deleteTaskById } = require("../controllers/taskController");
 const router = express.Router()
 
 router.post("/", createTask)
 router.get("/", getAllTasks)
 router.get("/:id", getTaskById)
+router.delete("/:id", deleteTaskById)
 
 module.exports = router;
