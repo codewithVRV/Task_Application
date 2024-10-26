72527c750d32992edd09ee4f3b101ceae52352c9
Vishnu Kumar <vermaji1891@gmail.com>
2024-10-26 11:55:37 +0530

    able to create task

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,4 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const CreatePost = () => {
+    const [data, setData] = useState({
+        title:"", description:""
+    })
+
+    console.log("form data is", data)
+    const navigator = useNavigate()
+    async function handleSubmitData (e) {
+        e.preventDefault()
+        console.log("function called!")
+        try{
+            const response = await axios.post("http://localhost:3000/api/tasks", data, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            console.log(response)
+            navigator("/")
+            
+        }
+        catch(error) {
+            console.log("Error caught by data submission", error)
+        }
+    }
   return (
     <>
       <div className="min-h-screen flex items-center justify-center bg-gray-100 mt-10 mb-10">
@@ -6,19 +33,20 @@ const CreatePost = () => {
           <h2 className="text-2xl font-bold mb-6 text-center">
             Create Post Here
           </h2>
-          <form>
+          <form onSubmit={handleSubmitData}>
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
                 htmlFor="email"
               >
-                Image Title
+                Task Title
               </label>
               <input
                 type="text"
                 id="title"
                 name="title"
-                placeholder="Enter your image title here"
+                onChange={(e) => setData({...data, title:e.target.value})}
+                placeholder="Enter your Task here"
                 className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-orange-500 focus:border-orange-300"
               />
             </div>
@@ -34,6 +62,7 @@ const CreatePost = () => {
                 type="text"
                 id="description"
                 name="description"
+                onChange={(e) => setData({...data, description:e.target.value})}
                 placeholder="Describe about you post here"
                 className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-orange-500 focus:border-orange-300"
               />
@@ -44,7 +73,7 @@ const CreatePost = () => {
                 type="submit"
                 className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               >
-                Register
+                Create Task!
               </button>
             </div>
           </form>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,55 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>We are at home page</h1>
-        </div>
-    )
-}
+  const [allTask, setAllTasks] = useState([]);
+
+  const fetchAllTask = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/tasks");
+      console.log("response is", response);
+      setAllTasks(response.data);
+    } catch (error) {
+      console.log("Error caught by fethallTask", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllTask();
+  }, []);
+  return (
+    <div className="flex justify-center flex-wrap gap-4 mt-12 mb-10">
+      {allTask &&
+        allTask.map((data) => {
+          return (
+            <div
+              key={data._id}
+              className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            >
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {data.title}
+                </h5>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                {data.description}
+              </p>
+              <div>
+                <button className="bg-blue-500 px-4 font-semibold text-white py-2 rounded-md">
+                  Edit
+                </button>
+                <button className="bg-blue-500 px-4 mx-4 font-semibold text-white py-2 rounded-md">
+                  Delete
+                </button>
+                <button className="bg-blue-500 px-4 mx-4 font-semibold text-white py-2 rounded-md">
+                  Pending
+                </button>
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 
 export default Home;
